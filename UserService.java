1e047b0d00f59745dc2ab336fc656010e3ffdb39
CloudTechDevOps <147053314+CloudTechDevOps@users.noreply.github.com>
2025-03-15 17:25:58 +0530

    Create UserService.java

---
+++ UserService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User addUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public Optional<User> updateUser(Long id, User user) {
+        return userRepository.findById(id).map(existingUser -> {
+            existingUser.setName(user.getName());
+            existingUser.setEmail(user.getEmail());
+            return userRepository.save(existingUser);
+        });
+    }
+
+    public boolean deleteUser(Long id) {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+}
